513c45f70b648383e91f5d86fd5ebd63f94a4943
ShadowyXP <maxschreifels32@gmail.com>
2024-03-19 21:10:34 -0500

    Replacing with boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const WILDSEA = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+WILDSEA.abilities = {
+  str: 'WILDSEA.Ability.Str.long',
+  dex: 'WILDSEA.Ability.Dex.long',
+  con: 'WILDSEA.Ability.Con.long',
+  int: 'WILDSEA.Ability.Int.long',
+  wis: 'WILDSEA.Ability.Wis.long',
+  cha: 'WILDSEA.Ability.Cha.long',
+};
+
+WILDSEA.abilityAbbreviations = {
+  str: 'WILDSEA.Ability.Str.abbr',
+  dex: 'WILDSEA.Ability.Dex.abbr',
+  con: 'WILDSEA.Ability.Con.abbr',
+  int: 'WILDSEA.Ability.Int.abbr',
+  wis: 'WILDSEA.Ability.Wis.abbr',
+  cha: 'WILDSEA.Ability.Cha.abbr',
+};
