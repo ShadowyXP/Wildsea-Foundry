c3df4187962548ab6bee4faf91bbd454e016f549
ShadowyXP <maxschreifels32@gmail.com>
2024-03-21 22:57:24 -0500

    Added .less support, and started on the track sheet.

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,7 @@
 export const WILDSEA = {};
 
-/**
- * The set of Ability Scores used within the system.
- * @type {Object}
- */
-WILDSEA.abilities = {
-  str: 'WILDSEA.Ability.Str.long',
-  dex: 'WILDSEA.Ability.Dex.long',
-  con: 'WILDSEA.Ability.Con.long',
-  int: 'WILDSEA.Ability.Int.long',
-  wis: 'WILDSEA.Ability.Wis.long',
-  cha: 'WILDSEA.Ability.Cha.long',
-};
-
-WILDSEA.abilityAbbreviations = {
-  str: 'WILDSEA.Ability.Str.abbr',
-  dex: 'WILDSEA.Ability.Dex.abbr',
-  con: 'WILDSEA.Ability.Con.abbr',
-  int: 'WILDSEA.Ability.Int.abbr',
-  wis: 'WILDSEA.Ability.Wis.abbr',
-  cha: 'WILDSEA.Ability.Cha.abbr',
+WILDSEA.AspectTypes = {
+	trait: "WILDSEA.AspectLabel.trait",
+	gear: "WILDSEA.AspectLabel.gear",
+	companion: "WILDSEA.AspectLabel.companion",
 };
